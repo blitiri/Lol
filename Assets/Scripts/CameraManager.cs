ef8f0b04e738aaf07edecc72cebf757871ab4b04
Marco <blitiri@gmail.com>
2017-03-23 21:34:58 +0100

    Corretto baco spostamento camera

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -18,23 +18,23 @@ public class CameraManager : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.Space) && (GameManager.instance.getSelectedObject() != null)) {
 			selectedObjectPosition = GameManager.instance.getSelectedObject().transform.position;
-			gameObject.transform.position = new Vector3 (selectedObjectPosition.x, gameObject.transform.position.y, selectedObjectPosition.z - 3);
+			transform.position = new Vector3 (selectedObjectPosition.x, gameObject.transform.position.y, selectedObjectPosition.z - 3);
 		} else {
 			viewportPoint = Camera.main.ScreenToViewportPoint (Input.mousePosition);
 			horizontalScroll = Input.GetAxis ("Mouse X");
 			verticalScroll = Input.GetAxis ("Mouse Y");
 			zoom = Input.GetAxis ("Mouse ScrollWheel");
 			if (Input.GetKey (KeyCode.D) || (viewportPoint.x > (1 - mouseMovementPadding))) {
-				gameObject.transform.Translate (new Vector3 (Mathf.Max (horizontalScroll, minScrollSpeed), 0, 0));
+				transform.Translate (new Vector3 (Mathf.Max (horizontalScroll, minScrollSpeed), 0, 0), Space.World);
 			}
 			if (Input.GetKey (KeyCode.A) || (viewportPoint.x < mouseMovementPadding)) {
-				gameObject.transform.Translate (new Vector3 (Mathf.Min (horizontalScroll, -minScrollSpeed), 0, 0));
+				transform.Translate (new Vector3 (Mathf.Min (horizontalScroll, -minScrollSpeed), 0, 0), Space.World);
 			}
 			if (Input.GetKey (KeyCode.W) || (viewportPoint.y > (1 - mouseMovementPadding))) {
-				gameObject.transform.Translate (new Vector3 (0, Mathf.Max (verticalScroll, minScrollSpeed), 0));
+				transform.Translate (new Vector3 (0, 0, Mathf.Max (verticalScroll, minScrollSpeed)), Space.World);
 			}
 			if (Input.GetKey (KeyCode.S) || ((viewportPoint.y < mouseMovementPadding * 3) && (viewportPoint.y > mouseMovementPadding * 2))) {
-				gameObject.transform.Translate (new Vector3 (0, Mathf.Min (verticalScroll, -minScrollSpeed), 0));
+				transform.Translate (new Vector3 (0, 0, Mathf.Min (verticalScroll, -minScrollSpeed)), Space.World);
 			}
 			if (zoom != 0) {
 				zoomDirection = (zoom > 0 ? 1 : -1);
