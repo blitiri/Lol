32ef5e6b4a24a27ec38ede38d42587a0e8142596
Carlo <carlo93.cg@gmail.com>
2017-04-04 21:50:46 +0200

    Ennesimo Merge

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public abstract class Abilities : ScriptableObject {
-<<<<<<< HEAD
-	public abstract void OnAbilityActivation(MonoBehaviour caller);
-=======
+
 	protected GameObject player;
 
 	public abstract void OnAbilityActivation();
 	public void SetPlayer(GameObject player){
 		this.player = player;
 	}
->>>>>>> 747a62680cd2731a678f76e8b71d80f25bd38099
 }
--- Assets/Scripts/Skills/BouncingBomb.cs
+++ Assets/Scripts/Skills/BouncingBomb.cs
@@ -10,15 +10,7 @@ public class BouncingBomb : Abilities {
 
 	public override void OnAbilityActivation(){
 		GameObject ball = Instantiate (ballPrefab) as GameObject;
-<<<<<<< HEAD
-
-
-		ball.transform.position = ballSpawnTransform.transform.position;
-		ball.transform.rotation = ballPrefab.transform.rotation;
-		ball.GetComponent<Rigidbody> ().AddForce (ballSpawnTransform.transform.forward * ballForce * Time.deltaTime, ForceMode.Impulse);
-=======
 		ball.transform.position = ballSpawn.position;
->>>>>>> 747a62680cd2731a678f76e8b71d80f25bd38099
 	}
 
 
